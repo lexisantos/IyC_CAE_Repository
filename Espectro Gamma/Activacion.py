d587894603ab6e40cb5e167dd80382efab7fb4e7
Alex Canchanya <89670292+lexisantos@users.noreply.github.com>
2024-05-30 16:08:15 -0300

    Update Activacion.py
    Chequeados NAA_calib, y el módulo Incognita

--- Espectro Gamma/Activacion.py
+++ Espectro Gamma/Activacion.py
@@ -10,15 +10,13 @@ from datetime import datetime
 import re
 import urllib.request
 
-year = 365.24219878
+day_year = 365.24219878
 s_day = 86400 
 
 tabla_RA3 = pd.read_excel('D:/Documentos RA-3/Copia de Listado de fuentes v13.xls',
                           sheet_name='Fuentes', index_col='Fuente')
 
-tabla_iaea = {'Abundance': {'Cu': 0.6915, 'Au': 1}, 'Sec_eff': {'Cu': 4.50e-24, 'Au': 98.65e-24},
-              't1/2': {'Cu': 45722, 'Au': 232770, 'Eu': 13.517}, 'Mr': {'Cu': 63.55, 'Au': 196.967}}
-              #abundancia %, seccion eff (n,g) [cm^-2], t1/2 del isótopo [s], Mr masa molar [g/mol]
+Mr = {'198Au': 44.01, }
 
 Livechart = "https://nds.iaea.org/relnsd/v1/data?"
 
@@ -37,25 +35,21 @@ def redondeo(mean, err, cs, texto = False):
         return (mean_R, '±',err_R)
     else:
         return (float(mean_R), float(err_R))
-    
-def poly(grado, an, x):
-    exps = np.arange(grado+1)
-    return np.array([np.dot(an[::-1], xx**exps) for xx in x])
-    
+       
 def ajuste_pol(grado, xdata, ydata, y_err=None):
     try:
         weight = 1/y_err
     except:
         weight = y_err
     coef, pcov = np.polyfit(xdata, ydata, deg = grado, w= weight, cov=True)
-    yfit = poly(grado, coef, xdata)
+    yfit = np.polyval(coef, xdata)
     residuals = ydata - yfit
     perr = np.sqrt(np.diag(pcov))
     ss_res = np.sum(residuals**2)
     ss_tot = np.sum((yfit-np.mean(yfit))**2)
     R2 = 1 - (ss_res / ss_tot)
     try: 
-        chi2 = (1/(len(residuals)-2))*np.sum((ydata*residuals*weight)**2)
+        chi2 = (1/(len(residuals)-1))*np.sum(residuals**2)
     except:
         chi2 = 0
     return coef, perr, R2, chi2, residuals
@@ -65,8 +59,54 @@ def lc_pd_dataframe(url):
     req.add_header('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:77.0) Gecko/20100101 Firefox/77.0')
     return pd.read_csv(urllib.request.urlopen(req))
 
+def select_data(measured, data_table, tolerance, idx=0): 
+    """
+    Es como un VLOOKUP
+
+    Parameters
+    ----------
+    measured : TYPE
+        DESCRIPTION.
+    data_table : TYPE
+        DESCRIPTION.
+    tolerance : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    rows, cols = len(measured), len(data_table.T)
+    data_sel = np.zeros((rows, cols-1))
+    for jj, en in enumerate(measured):
+        for ee in data_table:
+            x = en/ee[idx]
+            if 1-tolerance<x<1+tolerance:
+                data_sel[jj] = np.delete(ee, idx)
+    return data_sel
+
+def dif_rel(x1, x2):
+    return (2*(x1 - x2)/(x1 + x2))*100
+
+def Incognita(Fuente, Energias, cps_peaks, err_cps, poly_params, dt,
+              tolerancia: float=0.01):
+    data_iaea = loadfromIAEA(Fuente, 'decay')
+    data_doc = tabla_RA3.loc[Fuente][['Act       [Bq]', 'σ Act']].values.astype(float)
+    data_doc[1] = data_doc[1]*data_doc[0]
+    data_cal = data_doc*np.exp(np.log(2)*dt/data_iaea.get(['half_life_sec']).to_numpy().mean())
+    eiu = data_iaea.get(['energy', 'intensity', 'unc_i']).to_numpy()
+    peaks = select_data(Energias, eiu, tolerancia)/100
+    peaks[np.isnan(peaks)] = 0
+    effs = np.exp(np.polyval(poly_params, np.log(Energias)))
+    Act_calc = cps_peaks/(peaks[:, 0]*effs)
+    Err_calc = Act_calc*np.sqrt((err_cps/cps_peaks)**2 + (peaks[:, 1]/peaks[:, 0])**2)
+    diff = dif_rel(Act_calc, data_cal[0])
+    return np.array([Act_calc, Err_calc]).T, data_cal, diff
+
 class loadfromIAEA:
-    def __init__(self, nuclei, state, radiation_type: str='g', only_stable: bool=True):
+    def __init__(self, Fuente, state, radiation_type: str='g', only_stable: bool=True):
+        nuclei = re.split('-|_| ', Fuente)[0]
         path = {'decay': f"fields=decay_rads&nuclides={nuclei}&rad_types={radiation_type}",
                 'estable': f"fields=ground_states&nuclides={nuclei}"}
         df = lc_pd_dataframe(Livechart + path[state.lower()])
@@ -84,40 +124,32 @@ class loadfromIAEA:
 class NAA_calib:
     def __init__(self, Fuente, FechaCalib):
         self.fuente = Fuente
-        self.emisor = re.split('-|_| ', Fuente)[0]
-        self.datafromIAEA = loadfromIAEA(self.emisor, 'decay').data
+        self.datafromIAEA = loadfromIAEA(Fuente, 'decay').data
         self.datafromRA3 = tabla_RA3.loc[Fuente]
         self.fecha_cal = datetime(*FechaCalib)
         self.fecha_doc = self.datafromRA3['Fecha']
-        dt_caldoc = float((self.fecha_doc - self.fecha_cal).days)*s_day
-        intensity, unc_int, energy, hl = self.datafromIAEA.get(['intensity', 'unc_i', 'energy', 'half_life_sec']).values.T
-        self.halflife_s = np.mean(hl)
+        self.dt_caldoc = float((self.fecha_doc - self.fecha_cal).days)*s_day
+        intensity, unc_int, energy, hl = self.datafromIAEA.get(['intensity', 'unc_i', 'energy', 'half_life_sec']).to_numpy().T
+        self.halflife_s = hl.mean()
         self.act_doc = self.datafromRA3[['Act       [Bq]', 'σ Act']].values.astype(float)
-        self.act_cal = self.act_doc*np.exp(np.log(2)*dt_caldoc/self.halflife_s)
-        self.act_cal[1] = self.act_cal[1]*self.act_cal[0]
-        self.int_energy = np.transpose([energy, intensity])
-        self.int_err = unc_int #np.array([np.mean(hl), np.mean(self.datafromRA3.get('unc_hls'))])
+        self.act_doc[1] = self.act_doc[1]*self.act_doc[0]
+        self.act_cal = self.act_doc*np.exp(np.log(2)*self.dt_caldoc/self.halflife_s)
+        self.int_energy = np.transpose([energy, intensity, unc_int])
+        #np.array([np.mean(hl), np.mean(self.datafromRA3.get('unc_hls'))])
         # self.dt_caldoc = dt_caldoc
-    def cal_eff(self, NetCounts, Energias, grado_pol, tlive, NetCounts_err= None, criterio: float=0):
-        i_sel = self.int_energy[:, 1]>criterio
+    def cal_eff(self, NetCounts, Energias, grado_pol, tlive, 
+                NetCounts_err= None, criterio: float=0, tolerancia: float=0.01):
+        i_sel = self.int_energy[:, 1]>criterio*100
         I_E_IAEA = np.array(self.int_energy[i_sel])
-        I_err_sel = np.array(self.int_err[i_sel])
-        energy_sel = np.zeros(len(Energias))
-        intensity_sel = np.zeros(len(Energias))
-        int_err_sel = np.zeros(len(Energias))
-        for jj, en in enumerate(Energias):
-            for n, ee in enumerate(I_E_IAEA):
-                x = en/ee[0]
-                if 0.99<x<1.01:
-                    intensity_sel[jj] = ee[1]/100
-                    int_err_sel[jj] = I_err_sel[n]/100
-                    energy_sel[jj] = ee[0]
-        eff = NetCounts/(tlive*self.act_cal[0]*intensity_sel)
+        data_sel = select_data(Energias, I_E_IAEA, tolerancia)/100  
+        eff = NetCounts/(tlive*self.act_cal[0]*data_sel[:, 0])
         try:
-            eff_err = eff*np.sqrt((NetCounts_err/NetCounts)**2 + (int_err_sel*self.act_cal[0])**2 + (self.act_cal[1]*intensity_sel)**2)
+            eff_err = eff*np.sqrt((NetCounts_err/NetCounts)**2 + (data_sel[:, 1]/data_sel[:, 0])**2 
+                                  + (self.act_cal[1]/self.act_cal[0])**2)
                                   #np.exp(np.log(2)*self.dt_caldoc/self.halflife_s[0])*np.log(2))
         except:
             eff_err = None
-        coef, perr, R2, chi2, res = ajuste_pol(grado_pol, np.log(Energias), np.log(eff), y_err=eff_err)
-        return eff, eff_err, coef, perr, R2, chi2, np.array([energy_sel, intensity_sel]).T, res
- 
+        coef, perr, R2, chi2, res = ajuste_pol(grado_pol, np.log(Energias), np.log(eff), y_err=eff_err/eff)
+        self.eff_params = {'an': coef, 'an_err': perr, 'R-squared': R2, 'chi-square': chi2,
+                           'residuals': res, 'grado_pol': grado_pol}
+        return eff, eff_err, coef, perr, R2, chi2, data_sel, res
